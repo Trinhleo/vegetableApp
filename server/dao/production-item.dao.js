8c61b6d5bff644a0f15eb099e18f524810d1a7e6
trinh <hnkt2907@gmail.com>
2016-11-03 01:16:39 +0700

    add production item

---
+++ server/dao/production-item.dao.js
@@ -0,0 +1,75 @@
+
+var mongoose = require('mongoose');
+
+require('../models/productionItem.model.js');
+var ProductionItem = mongoose.model('ProductionItem');
+var _ = require('lodash');
+
+module.exports = {
+    listAllProductionItems: listAllProductionItems,
+    readProductionItemById: readProductionItemById,
+    readProductionItem: readProductionItem,
+    createProductionItem: createProductionItem,
+    updateProductionItem: updateProductionItem,
+    deleteProductionItem: deleteProductionItem
+};
+
+function listAllProductionItems(callback) {
+    ProductionItem.find({}, function (err, result) {
+        if (err) {
+            return callback(err, null);
+        }
+        callback(null, result);
+    });
+}
+
+function readproductionItemById(id, callback) {
+    ProductionItem.findById(id, function (err, result) {
+        if (err) {
+            return callback(err, null);
+        }
+        callback(null, result);
+    });
+}
+
+function readProductionItem(productionItem, callback) {
+    ProductionItem.findOne({
+        name: productionItem.name
+    }, function (err, result) {
+        if (err) {
+            return callback(err, null);
+        }
+        callback(null, result);
+    });
+}
+
+function createProductionItem(info, callback) {
+    var productionItem = new productionItem(info);
+
+    productionItem.save(function (err, result) {
+        if (err) {
+            return callback(err, null);
+        }
+        callback(null, result);
+    });
+
+}
+
+function updateProductionItem(id, info, callback) {
+
+    productionItem.findByIdAndUpdate(id, info, function (err, result) {
+        if (err) {
+            return callback(err, null);
+        }
+        callback(null, true);
+    });
+}
+
+function deleteproductionItem(id, callback) {
+    productionItem.findByIdAndRemove(id, function (err, result) {
+        if (err) {
+            return callback(err, null);
+        }
+        callback(null, true);
+    });
+}
